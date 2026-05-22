7f2d6647fdd509c574a7efb0fa86f9d44a5ad4a1
Planet Doge <142177581+PlanetDogeCodes@users.noreply.github.com>
2026-05-22 15:04:21 -0600

    Delete PocketCompiler directory

--- PocketCompiler/include/fs_io.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdbool.h>
-#include <stddef.h>
-
-bool fs_make_app_dirs(void);
-bool fs_save_text(const char* path, const char* text);
-bool fs_load_text(const char* path, char* out, size_t out_size);
